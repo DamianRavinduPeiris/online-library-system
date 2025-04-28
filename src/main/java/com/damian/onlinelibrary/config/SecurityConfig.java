7510cf592b1568367ff429ea5d47c466557872d1
DamianRavinduPeiris <drpeiris3@gmail.com>
2025-04-28 16:07:38 +0530

    Added basic auth for user related endpoints and allowed book related endpoints.

---
+++ src/main/java/com/damian/onlinelibrary/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.damian.onlinelibrary.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/v1/books/**").permitAll() // Allow access to book collection
+                .requestMatchers("/api/v1/users/**").authenticated() // Secure User API
+                .and()
+                .httpBasic(); // Use basic authentication
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+}
