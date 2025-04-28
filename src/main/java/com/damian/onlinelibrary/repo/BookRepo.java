8a36f51f5a621c31f5e3802175b0f2feb1d1b400
DamianRavinduPeiris <drpeiris3@gmail.com>
2025-04-28 16:29:16 +0530

    Fixed a bug in the BookRepo.

--- src/main/java/com/damian/onlinelibrary/repo/BookRepo.java
+++ src/main/java/com/damian/onlinelibrary/repo/BookRepo.java
@@ -4,12 +4,12 @@ import com.damian.onlinelibrary.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import java.util.Optional;
+import java.util.List;
 
 @EnableJpaRepositories
 public interface BookRepo extends JpaRepository<Book,String> {
     /*Fetches Books by Author.*/
-    Optional<Book> findByAuthor(String author);
+   List<Book> findByAuthor(String author);
     /*Fetches Books by published year.*/
-    Optional<Book> findByPublishedYear(String publishedYear);
+    List<Book> findByPublishedYear(String publishedYear);
 }
